a763027f3f57caf601287a13eefa23e3ef5aa237
Stephan Shere <sshere@exqsd.com>
2018-03-16 06:38:33 -0700

    adding source and example file.

---
+++ main.py
@@ -0,0 +1,17 @@
+import urllib
+import urllib2
+import requests
+
+lines = open("plates.txt").readlines();
+for line in lines:
+ word = (line.rstrip())
+ method = "POST"
+ handler = urllib2.HTTPHandler()
+ opener = urllib2.build_opener(handler)
+ data = dict(plateChoice='001', choice=word)
+ data = urllib.urlencode(data)
+ request = requests.post('https://servicearizona.com/webapp/vehicle/plates/personalizedChoiceSearch.do', data=data, headers={'Content-Type':'application/x-www-form-urlencoded'})
+ data = request.text
+ if "is not available." not in data:
+	print(word + " IS AVAILABLE");
+
